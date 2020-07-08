ca9ff1b25667d5e4748eb339ae24032a7c97157d
atollett <a.t.tollett@gmail.com>
2020-07-08 12:29:40 -0700

    fixed double call to initials

--- script.js
+++ script.js
@@ -35,33 +35,45 @@ var direction = 0;
 var time = 10;
 var timeInterval;
 var scores = [];
+var isFinal = true;
 
+//begins quiz
 startButton.onclick = function() { 
     event.preventDefault();
    
     document.getElementById("start").style.display = "none"
     
+    //starts timer
     startTime();
-
+    //sets up quiz
     buildQuiz();
 };
 
 
-
+//determines time interval
 function startTime() {
-    timeInterval = setInterval(clockTick, 1000);
+    if(isFinal) {
+        timeInterval = setInterval(clockTick, 1000);
+    }
+    return;
   }
 
+  //decrements timer and ouputs it to screen
 function clockTick() {
     time--;
     document.getElementById("txt").textContent = "Seconds: " + time;
 
     if(time <= 0) {
-        changeTime(0);
+        time = 0;
+        isFinal = false;
+        finalPage();
+        return;
     }
 }
 
+//creates questions and possible answers
 function buildQuiz(){
+    //retrieves array from JSON
     var questions = JSON.parse(localStorage.getItem("questions"));
     var ul = document.createElement("ul");
     var div = document.createElement("div");
@@ -69,12 +81,13 @@ function buildQuiz(){
 
     body.appendChild(ul);
 
-    
+    //creates question
     var h1 = document.createElement("h1");
     h1.textContent = questions[j].title;
     div.appendChild(h1);
     quizContainer.appendChild(div);
 
+    //creates button options
     for(var i = 0; i < 4; ++i) {
         var button = document.createElement("button");
         button.textContent = questions[j].choices[i];
@@ -83,37 +96,52 @@ function buildQuiz(){
         ul.appendChild(button);
             
 
-        button.onclick = questionClick;        
+        button.onclick = questionClick;
+        
+        if(!isFinal) {
+            return;
+        }
     }
 
     div.appendChild(ul);
 }
 
+//determines whether button click was correct, if questions still remain, calls build quiz again
 function questionClick() {
     //console.log(this.textContent === questions[j].answer);
 
     if(this.textContent === questions[j].answer) {
+        //correct, either triggers final page if last question or builds quiz again with new question
         alert("You're right!");
         document.getElementById(j).style.display = "none";
         j++;
         if(j >= questions.length) {
             document.getElementById(j).style.display = "none";
-            changeTime(0);
+            isFinal = false;
+            finalPage();
+            return;
         } else {
             buildQuiz();
             console.log(j);
         }
         
     } else {
+        //decrements five seocnds from timer if question is incorrect, triggers final page if no more questions
         alert("You're wrong.");
 
         changeTime(-5);
+
+        if(!isFinal) {
+            return;
+        }
 
         document.getElementById(j).style.display = "none";
         j++;
         if(j >= questions.length) {
             document.getElementById(j).style.display = "none";
-            changeTime(0);
+            isFinal = false;
+            finalPage();
+            return;
         } else {
             buildQuiz();
             console.log(j);
@@ -126,10 +154,13 @@ function changeTime(direction) {
     time += direction;
     if(time <= 0) {
         time = 0;
+        isFinal = false;
         finalPage();
+        return;
     }
 }
 
+//outputs score and prompts for initials, stores in local storage
 function finalPage() {
     clearInterval(timeInterval);
 
@@ -152,6 +183,7 @@ function finalPage() {
 
 }
 
+//prints highscores
 function printScores() {
     var highScores = JSON.parse(localStorage.getItem("high-scores"));
     
